86f642eb7d41e70b69454c29d25071a1a199ac1b
Pedrohmenezes <pedro.mk.13@hotmail.com>
2025-04-23 16:15:22 -0300

    Ajuste pasta raiz

--- backend/GTInventory.Domain/obj/Debug/net9.0/GTInventory.Domain.AssemblyInfo.cs
+++ backend/GTInventory.Domain/obj/Debug/net9.0/GTInventory.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTInventory.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511f971b96f78b055466046a987a74e3534f2082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427edc96209488117f31fce4d4f8ce540a85daf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTInventory.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTInventory.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
